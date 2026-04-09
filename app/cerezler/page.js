395015c42c3acf8ed53727d5a3618463e3afcb0f
BBQ-dev <ekucukoglu.teknikservis@gmail.com>
2026-04-10 00:14:09 +0300

    Update page.js

--- app/cerezler/page.js
+++ app/cerezler/page.js
@@ -1,9 +1,1 @@
-import React from 'react';
 
-export default function Cerezler() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-100 text-[#1E40AF] font-bold text-2xl">
-      Çerezler Sayfası - Yapım Aşamasında
-    </div>
-  );
-}
